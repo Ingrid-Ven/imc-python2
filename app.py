53e1655c16f232275cd3d64e841d6b2cce29f47e
Ingrid Venancio <ingrid.venancioo@gmail.com>
2026-04-23 21:41:58 -0300

    terminando conexão com BD

--- app.py
+++ app.py
@@ -4,10 +4,36 @@ from db import execute_query
 app = Flask(__name__)
 app.secret_key = 'imc_secret_key_2026'
 
+def calcular_imc(peso,altura):
+    return round(peso / (altura ** 2), 2)
+    
+def classificacao(imc):
+    
+    if imc < 18.5:
+        classificacao = 'Abaixo do peso'
+    elif imc < 25:
+        classificacao = 'Peso normal'
+    ## adicionar as demais classificações
+    else:
+        classificacao = 'Erro no cálculo do IMC'
+
+
+
 
 @app.route('/')
 def index():
-    sql =  "SHOW TABLES;"
+    sql =  '''
+CREATE TABLE IF NOT EXISTS calculos(
+    id_calculo BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    peso DECIMAL(6,2) NOT NULL,
+    altura DECIMAL(5,2) NOT NULL,
+
+    criado_em DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    alterado_em DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletado_em DATETIME NULL
+);    
+'''
     resultado = execute_query(sql, fetch=True)
     print(resultado)
 
@@ -16,35 +42,59 @@ def index():
 
 @app.route('/resultados')
 def resultados():
-    calculos = []
-    return render_template('resultados.html', calculos=calculos, total=len(calculos))
+    
+    try:
+        sql = "SELECT * FROM calculos WHERE deletado_em IS NULL;"
+
+        calculos = execute_query(sql, fetch=True)
+
+        if calculos is None:
+            calculos = []
+
+    except Exception as e:
+        flash(f'Erro ao buscar dados!', 'danger')
+        app.logger.error(f'Erro no SELECT: {e}')
+        return redirect(url_for('resultados'))
+
+
+    return render_template('resultados.html', calculos=calculos, total=len(calculos), calcular = calcular_imc, classificacao = classificacao)
 
 
 @app.route('/calcular', methods=['GET', 'POST'])
 def calcular():
     
     if request.method == 'POST':
-        nome = request.form.get('nome','Não foi enviado um nome')
-        peso = request.form.get('peso')
-        altura = request.form.get('altura')
+        nome = request.form.get('nome','Não foi enviado um nome').strip()
+        peso = request.form.get('peso').strip()
+        altura = request.form.get('altura').strip()
 
         peso = float(peso)
         altura = float(altura)
 
-        imc = round(peso / (altura ** 2), 2)
+        try:
+            #Cria o SCRIPT SQL para ser enviado, %s é cada valor
+            sql = 'INSERT INTO calculos(nome, peso, altura) VALUES (%s, %s, %s);'
 
-        if imc < 18.5:
-            classificacao = 'Abaixo do peso'
-        elif imc < 25:
-            classificacao = 'Peso normal'
-        ## adicionar as demais classificações
-        else:
-            classificacao = 'Erro no cálculo do IMC'
+            #Passa o SQL + os parametros que aqui são os dados em uma lista
+            execute_query(sql, (nome, peso, altura))
+
+            #Gera a notificação de sucesso.
+            flash(f'Produto [{nome}] cadastrado com sucesso!', 'success')
+
+            
+        
+
+            #Leva a tela de resultados
+            return redirect(url_for('resultados'))
+
+        except Exception as e:
+            flash(f'Erro ao salvar!', 'danger')
+            app.logger.error(f'Erro no INSERT: {e}')
+            return redirect(url_for('calcular'))
+
+
         
         flash(f'Olá {nome}, seu IMC é: {imc} - Classificação: {classificacao}', 'success')
-
-        return redirect(url_for('resultados'))
-
 
     return render_template('formulario.html')
 
